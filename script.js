f98aee681d7a1aeaea098cd32947c94515a3e9fa
Viranti2312 <2300016047@webmail.uad.ac.id>
2024-10-22 09:13:25 +0700

    Attempt 1 - 10.30

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const generatePassword = (length, options) => {
   const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
 
   // TODO: Create a variable for the character set based on selected options
+  let characterPool = "";
   if (options.includeUppercase) characterPool += uppercase;
   if (options.includeLowercase) characterPool += lowercase;
   if (options.includeNumbers) characterPool += numbers;
